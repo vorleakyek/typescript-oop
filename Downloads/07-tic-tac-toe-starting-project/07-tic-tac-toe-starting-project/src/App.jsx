f21880c237e04fb01beed49b9acf14749189a119
Vorleak Yek <vorleakyek@gmail.com>
2024-03-04 21:33:22 -0800

    initial commit

---
+++ Downloads/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>
+          <span className="player-symbol">O</span>
+        </li>
+      </ol>
+
+
+    </div>
+  </main>
+}
+
+export default App
